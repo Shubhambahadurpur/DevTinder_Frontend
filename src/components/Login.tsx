62da86d4d5bb00d5c5dafcd08aadc20f596a66fd
Shubham Kumar <shubhambahadurpur48@gmail.com>
2025-07-30 17:36:51 +0530

    feat: Implement login functionality with validation and error handling
    - Added axios for making HTTP requests.
    - Introduced validator for email and password validation.
    - Created state management for login data and error messages.
    - Implemented form submission with error handling.
    - Enhanced UI for the login form with appropriate labels and error display.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,87 @@
+import axios from "axios";
+import { useState } from "react";
+import validator from 'validator';
+
+interface LoginData {
+  email: string,
+  password: string
+}
 
 const Login = () => {
+  const [loginData, setLoginData] = useState<LoginData>({ email: '', password: '' });
+  const [errors, setErrors] = useState<string>('')
+
+  const changeEmail = (value: string) => {
+    if (value.trim() === '') {
+      setLoginData((prev) => ({ ...prev, email: '' }));
+      return
+    }
+    else {
+      setLoginData((prev) => ({ ...prev, email: value.trim() }));
+    }
+  }
+
+  const changePassword = (value: string) => {
+    if (value.trim() === '') {
+      setLoginData((prev) => ({ ...prev, password: '' }));
+      return
+    }
+    else {
+      setLoginData((prev) => ({ ...prev, password: value.trim() }));
+    }
+  }
+
+  const submitForm = async () => {
+    try {
+      await axios.post('http://localhost:3000/login', { emailId: loginData.email, password: loginData.password}, { withCredentials: true})
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      console.error("ERROR while logging in")
+    }
+  }
+
+  const validateFormData = () => {
+    if (!validator.isEmail(loginData.email)) {
+      setErrors("Please Enter a valid email id");
+    }
+    else if (!validator.isStrongPassword(loginData.password)) {
+      setErrors("Please Enter a strong password");
+    }
+
+    else {
+      submitForm();
+    }
+
+  }
+
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-      <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <div className="flex justify-center mt-15">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title text-2xl flex justify-center">Login</h2>
+          <label htmlFor="email" className="font-medium text-xl">Email</label>
+          <input type="text" id='email' value={loginData.email} onChange={(e) => changeEmail(e.target.value)} className="input" placeholder="Enter your Email id" />
+
+          <label htmlFor="password" className="font-medium text-xl mt-2">Password</label>
+          <input type="password" id='password' value={loginData.password} onChange={(e) => changePassword(e.target.value)} className="input" placeholder="Enter your Password" />
+        {errors && <div className="text-red-500">{errors}</div>}
         </div>
+
+
+        <div className="card-actions justify-center mb-5">
+          <button className="btn btn-accent" onClick={validateFormData}>Login</button>
+        </div>
+
+        <div className="flex justify-center"> New User? <a rel="stylesheet" href="/signup" className="text-blue-500 ml-2"> Sign Up </a> </div>
       </div>
+      {/* <div className="toast toast-top toast-end">
+  <div className="alert alert-info">
+    <span>New mail arrived.</span>
+  </div>
+  <div className="alert alert-success">
+    <span>Message sent successfully.</span>
+  </div>
+</div> */}
     </div>
   )
 }
